127c7f88b32b6592e98046078883a93382c71a0f
Pasandi123 <pasandihettiarachchi22@gmail.com>
2025-03-31 04:06:44 +0530

    add buttons

--- startpage.js
+++ startpage.js
@@ -2,6 +2,17 @@ function startGame() {
     window.location.href = 'levelpage.html';
     alert(`Choose Difficulty Level`);
 }
+
+function profile() {
+    window.location.href = 'profile.html';
+    alert(`Successfully Open User Profile`);
+}
+
+function leaderBoard() {
+    window.location.href = 'leaderboard.html';
+    alert(`Successfully Open LeaderBoard`);
+}
+
 function exitGame(){
     if(confirm('Are you sure you want to exit?')) {
         window.close();
