29f8711ed0031b64f237e0895ac312eb51cc0772
Pasandi123 <pasandihettiarachchi22@gmail.com>
2025-03-31 18:27:40 +0530

    add go to menu button

--- levelpage.js
+++ levelpage.js
@@ -3,3 +3,6 @@ function selectLevel(button){
     window.location.href = `gamepage.html?button=${button}`;
 }
 
+document.getElementById("gotomenu").addEventListener("click", () => {
+    window.location.href="startpage.html";
+});
