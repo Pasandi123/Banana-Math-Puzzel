5cc8695c4f76af9a8801073d853832d4088febc3
Pasandi123 <pasandihettiarachchi22@gmail.com>
2025-04-01 14:08:12 +0530

    update levelpage.js

--- levelpage.js
+++ levelpage.js
@@ -1,6 +1,16 @@
-function selectLevel(button){
-    alert(`Starting Level ${button}!`);
-    window.location.href = `gamepage.html?button=${button}`;
+function selectLevel(easyButton){
+    alert(`Starting Level ${easyButton}!`);
+    window.location.href = `gamepage.html?button=${easyButton}`;
+}
+
+function selectLevel(mediumButton){
+    alert(`Starting Level ${mediumButton}!`);
+    window.location.href = `gamepage2.html?button=${mediumButton}`;
+}
+
+function selectLevel(hardButton){
+    alert(`Starting Level ${hardButton}!`);
+    window.location.href = `gamepage3.html?button=${hardButton}`;
 }
 
 document.getElementById("gotomenu").addEventListener("click", () => {
